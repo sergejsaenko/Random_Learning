7611d9f0f9d1e7885cd4e9f254d9f2df566e4413
sergejsaenko <ssaenko@live.de>
2023-10-18 16:05:03 +0200

    init
    testing assembly on macos

---
+++ Assembly/test.asm
@@ -0,0 +1,14 @@
+.data
+    hello db "Hello, World!",10,0
+.text
+    global _start
+_start:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, hello
+    mov edx, 13
+    int 0x80
+
+    mov eax, 1
+    xor ebx, ebx
+    int 0x80
